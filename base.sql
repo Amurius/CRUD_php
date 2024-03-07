ce5190207901a578e2b072e89eae1b8fb63515c1
Amurius <artuslr78@gmail.com>
2024-03-07 12:47:18 +0100

    navBar

---
+++ base.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE animaux;
+
+USE animaux;
+
+CREATE TABLE animal (id int not null primary key AUTO_INCREMENT, nom varchar(100), famille varchar(100), poids varchar(100), taille varchar(100), couleur varchar(100)) ;
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styleIndex.css">
+  <title>Animaux Farfelus</title>
+</head>
+
+<body>
+  <div id="page">
+    <nav>
+      <a href="">Accueil</a>
+      <a href="">Ajouter</a>
+      <a href="">Connexion</a>
+    </nav>
+    <div id="contenu">
+      <div class="divAnimal">
+
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
